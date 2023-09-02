17f1e7ded97ead94e93c83d640672c7255c26f5c
Herjus <herjus420@gmail.com>
2023-09-02 15:27:57 +0200

    bedre thread page

--- app/t/[id]/page.tsx
+++ app/t/[id]/page.tsx
@@ -1,4 +1,8 @@
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
 import prisma from '@/lib/prismadb';
+import Link from 'next/link';
 
 async function getThread(title: string){
     try{
@@ -25,13 +29,36 @@ export default async function ThreadPage({ params }: any){
     return(
         <div>
             {thread ? (
-                <div className='flex flex-col'>
-                    <span>t/{thread.title}</span>
-                    <span>{thread.description}</span>
+                <div className='space-y-3'>
+                    <div className='flex justify-between items-center'>
+                        <div className='flex flex-col'>
+                            <span className='font-bold text-xl'>t/{thread.title}</span>
+                            <span className='font-light text-md'>{thread.description}</span>
+                        </div>
+                        <Button>
+                            New post
+                        </Button>
+                    </div>
+                    <Separator/>
+                    <div className='space-y-2'>
+                        <Link href={`/`}>
+                            <Card>
+                                <CardContent className='flex flex-row p-4 space-x-5'>
+                                    <div className='flex flex-col w-2/6 md:w-1/6'>
+                                        <span className='font-semibold'>Post name</span>
+                                        <span className='font-light'>User</span>
+                                    </div>
+                                    <span className='flex-1 line-clamp-2 font-extralight'>I love anime tiddies</span>
+                                </CardContent>
+                            </Card>
+                        </Link>
+                        
+                    </div>
                 </div>
+                
             ) : (
                 <div className='flex flex-col'>
-                    <span>Thread "{params.id}" doesn't exist.</span>
+                    <span>Thread "t/{params.id}" doesn't exist.</span>
                 </div>
             )}
         </div>
