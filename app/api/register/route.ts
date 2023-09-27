27fc468e3a3f05505076b1bea8c68dbeb4b899dd
Herjus <herjus420@gmail.com>
2023-09-27 13:23:42 +0200

    maybe?

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 import prisma from "@/lib/prismadb";
 import { NextResponse } from "next/server";
 
-export async function POST(
+export default async function POST(
     request: Request
 ) {
     const body = await request.json();
--- app/api/thread/leave/route.ts
+++ app/api/thread/leave/route.ts
@@ -2,7 +2,7 @@ import getCurrentUser from "@/components/actions/getCurrentUser";
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prismadb";
 
-export async function POST(
+export default async function POST(
     request: Request
 ) {
     const body = await request.json();
